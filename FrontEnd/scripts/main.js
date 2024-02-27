bf8c0fc9dbc44cfc8c0230c83458fed0426656b9
LucMagg <luc.maggiotto@gmail.com>
2024-02-28 00:32:11 +0100

    feat js : ajout du mode édition
    ajout du header sticky "Mode édition", du bouton "Modifier" pour les projets et suppression des filtres une fois log (tout en js)
    suppression de tout ça et remise à la normale une fois délog

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -2,16 +2,92 @@
 
 const serverURL = "http://localhost:5678/api/";
 const api_works = "works";
+const works = await getContents(api_works);
+const filtres = await getCategories(works);
 
 let token = window.localStorage.getItem("token");
 check_token();
 
-const works = await getContents(api_works);
-const filtres = await getCategories(works);
-afficheFiltres(filtres);
 afficheGallery(works);
 setListenerToFilters();
 setOnCloseListener();
+
+
+function loginFunction(loginLink) {
+    /* Gestion du log in */
+    loginLink.innerHTML = "logout";
+    loginLink.setAttribute("href","#");
+
+    /* Ajout de la barre du mode édition */
+    let header = document.querySelector("header");
+    let headerContainer = document.querySelector(".header-container");
+    headerContainer.style.paddingTop = "50px";
+    
+    /* Création des éléments de la barre d'édition */
+    let fixedEditionBar = document.createElement("div");
+    fixedEditionBar.setAttribute("id","mode-edition");
+    fixedEditionBar.style.cssText = "position: fixed; left: 0; height: 3.75em; width: 100%; display: flex; flex-direction: row; justify-content: center; align-items: center; background-color: #000000; color: #FFFFFF;";
+
+    let editionParagraph = document.createElement("span");
+    editionParagraph.style.paddingLeft = "1em";
+    editionParagraph.innerHTML = "Mode édition";
+
+    let iconInverted = document.createElement("i");
+    iconInverted.setAttribute("class","fa-regular fa-pen-to-square");
+    iconInverted.setAttribute("aria-hidden","true");
+    iconInverted.style.cssText = "color: #FFFFFF;";
+
+    /* Création du lien de modification des projets */
+    let projetContainer = document.querySelector("#title-projets-container");
+
+    let lienModifier = document.createElement("a");
+    lienModifier.setAttribute("id","modification");
+    lienModifier.setAttribute("href","");
+    lienModifier.style.cssText = "padding-left: 2em; padding-top: 0.6em;"
+    let icon = document.createElement("i");
+    icon.setAttribute("class","fa-regular fa-pen-to-square");
+    icon.setAttribute("aria-hidden","true");
+    let modifier = document.createElement("span");
+    modifier.style.cssText = "padding-left: 1em; padding-top: 0;";
+    modifier.innerHTML = "Modifier";
+
+    /* Ajout dans le DOM */
+    header.insertBefore(fixedEditionBar, headerContainer);
+    fixedEditionBar.appendChild(iconInverted);
+    fixedEditionBar.appendChild(editionParagraph);
+
+    projetContainer.appendChild(lienModifier);
+    lienModifier.appendChild(icon);
+    lienModifier.appendChild(modifier);
+
+    /* Suppression des filtres */
+    let filterContainer = document.querySelector(".filters");
+    filterContainer.replaceChildren();
+
+    setModifListener();
+}
+
+
+function logoutFunction(loginLink) {
+    /* Gestion du log out */
+    window.localStorage.removeItem("token");
+    token = null;
+    loginLink.innerHTML = "login";
+    loginLink.setAttribute("href","login.html");
+
+    /* Suppression de la barre d'édition */
+    let fixedEditionBar = document.querySelector("#mode-edition");
+    fixedEditionBar.remove();
+
+    let headerContainer = document.querySelector(".header-container");
+    headerContainer.removeAttribute("style");
+
+    /* Suppression du lien de modification des projets */
+    let modifLink = document.querySelector("#modification");
+    modifLink.remove();
+
+    afficheFiltres(filtres);
+}
 
 
 function check_token() {
@@ -19,18 +95,16 @@ function check_token() {
 
     /* gestion du lien login/logout en fonction de la présence du token */
     if (token !== null) {
-        loginLink.innerHTML = "logout";
-        loginLink.setAttribute("href","#");
+        loginFunction(loginLink);
+    } else {
+        afficheFiltres(filtres);
     }
 
     /* ajout de l'évènement logout */
     loginLink.addEventListener('click', (event) => {
         if (loginLink.innerHTML === "logout") {
             event.preventDefault();
-            window.localStorage.removeItem("token");
-            token = null;
-            loginLink.innerHTML = "login";
-            loginLink.setAttribute("href","login.html");
+            logoutFunction(loginLink);
         }
     });
 
@@ -117,5 +191,17 @@ function setOnCloseListener() {
     window.addEventListener('beforeunload', (event) => {
         window.localStorage.removeItem(api_works);
         window.localStorage.removeItem("token");
+    });
+}
+
+
+function setModifListener() {
+    let modifLink = document.querySelector("#modification");
+
+    modifLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        /*** TODO ***/
+        /* Affiche la modale */
+        /* add listener sur les corbeilles et le bouton ajouter */
     });
 }
